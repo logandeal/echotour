ba6c66d4221d0d6f3e543c0998feee7f75be17c1
KatelynVanDyke <kvandyketech@gmail.com>
2022-11-05 12:34:43 -0500

    created node.js server

---
+++ public/script.js
@@ -0,0 +1,24 @@
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function toBlueMic(time) {
+    await sleep(time);
+    document.getElementById("mic").src = "blue_mic.png";
+}
+  
+async function toWhiteMic(time) {
+    await sleep(time);
+    document.getElementById("mic").src = "white_mic.png";
+}
+
+async function testColorChange() {
+    await sleep(2000);
+    toBlueMic();
+    await sleep(2000);
+    toWhiteMic();
+};
+
+function changePrompt(text) {
+    document.getElementById("prompt").innerHTML = text;
+}
