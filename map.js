55b090917a1952e59169d71ad458332301fbac69
Tristen Brown <tristenbrown314@gmail.com>
2022-11-06 02:47:56 -0600

    small addition to text

--- map.js
+++ map.js
@@ -39,16 +39,15 @@ class Person {
 //declaring nodes
 var root = new Node("the main entrance.", null, null, null, null);
 var node2 = new Node("a staircase with 12 steps going down.", "a hall that leads to the Interactive digital environments lab, E1419 Auditorium, and Mechanical and Aerospace.", "potential energy cafe.", "the front entrance.", "In this area there is a study area with couches, chairs, tables, and a counter!");
-var node3 = new Node("a long hallway that leads to the west wing and labs C1201 - C1206.", "an elevator, restrooms, labs C1210 - C1215, and an exit to the north couryard.", "a hallway that leads to the east wing, Overholser Atrium, labs C1243 - C1246, and labs C1249 - C1251.", "stairs with 12 steps leading up to potential energy cafe and the main entrance", "This is the portion of the main hallway closest to the entrance.");
+var node3 = new Node("the main hallway that leads to the west wing and labs C1201 - C1206.", "an elevator, restrooms, labs C1210 - C1215, and an exit to the north couryard.", "a hallway that leads to the east wing, Overholser Atrium, labs C1243 - C1246, and labs C1249 - C1251.", "stairs with 12 steps leading up to potential energy cafe and the main entrance", "This is the portion of the main hallway closest to the entrance.");
 var node4 = new Node("a hallway that leads to labs C1243 - C1246, Overholser Atrium, and Labs W1065 - W1081.", "a hallway that leads to the main entrance.", null, "a hallway that leads to restrooms, an elevator, the Civil and Environmental area, and labs C1249 - C1251", null);
 var node5 = new Node(null, "a hallway that leads to the interactive digital environments lab and an exit.", "a path that leads to the main entrance", "a hallway that leads to the mechanical and aerospace area and labs E1403, E1404, E1411, E1413, and E1414.", null);
 var node6 = new Node("a hall that leads to the interactive digital environments lab and the main entrance", "is a hallway that leads to labs E1411, E1413, E1414, and mechanical and aerospace.", "a womens restroom and labs E1403 and E1404.", "This portion of the hallway houses the Measurement Lab, the Thermal Fluids Lab, and the women's restroom.");
 var node7 = new Node("a hallway that leads to an elevator, restrooms, ketchum auditorium, student services, and administration - deans office.", "a hallway that leads to labs C1201 - C1204.", "a hallway that leads to bas W1065 - W1081, and an exit.", "a hallway that leads to the main entrance.", null);
 var node8 = new Node(null, "an area next to the main entrance.", null, null, "This is Potential Energy Cafe! You can buy coffee and snacks!");
-//maybe change left text
-var node9 = new Node(null, null, "the central hallway", null, "Here there are restrooms, an elevator, labs C1210-C1215 and an exit to the north courtyard");
+var node9 = new Node(null, null, "the main hallway", null, "Here there are restrooms, an elevator, labs C1210-C1215 and an exit to the north courtyard");
 var node10 = new Node("a hallway that leads to labs C1243 - C1246.", null, "a hallway that leads to restrooms, an elevator, the Civil and Environmental area, and labs C1249 - C1246", null, null);
-var node11 = new Node("labs C1249 - C1251.", null, null, "the Civil and Environmental area", null);
+var node11 = new Node("labs C1249 - C1251.", null, null, "the Civil and Environmental area", "There is a small study area here with a vending machine and restrooms.");
 var node12 = new Node("a hallway that leads to labs C1249 - C1251 and restrooms.", null, null, null, "This is the Civil and Environmental area. In front of you is the room where we made this video tour!");
 var node13 = new Node("the west wing", null, "an exit.", null, null);
 var node14 = new Node("a hall that leads to W1065 - W1081.", "a hall that leads to W1065 - W1081.", "an exit.", "labs C1249 - C1251", "Multiple paths lead to W1065 - W1081 because you can access the classrooms from both sides of the hall.");
